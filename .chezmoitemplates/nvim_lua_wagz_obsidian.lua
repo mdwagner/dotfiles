aef26426bca4b94c2a660202a508a306db00c686
Michael Wagner <mdwranger@gmail.com>
2024-01-29 10:51:25 -0500

    fix: disable obsidian on windows

--- .chezmoitemplates/nvim_lua_wagz_obsidian.lua
+++ .chezmoitemplates/nvim_lua_wagz_obsidian.lua
@@ -5,11 +5,13 @@ if vim.fn.has("mac") == 1 then
 	workspace_path = "~/Documents/My Vault"
 end
 
-obsidian.setup({
-  workspaces = {
-    {
-      name = "personal",
-      path = workspace_path,
-    },
-  },
-})
+if vim.fn.has("win32") == 0 then
+	obsidian.setup({
+		workspaces = {
+			{
+				name = "personal",
+				path = workspace_path,
+			},
+		},
+	})
+end
