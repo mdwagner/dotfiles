da2e5b04781f69d9889108bf7ddf484cd4784ebc
Michael Wagner <mdwranger@gmail.com>
2025-01-02 14:09:16 -0500

    feat: mise config diff
    chore: clean up default* files

--- private_dot_config/nvim/init.lua
+++ private_dot_config/nvim/init.lua
@@ -26,6 +26,7 @@ vim.opt.foldenable = false
 
 vim.g.mapleader = ","
 vim.g.maplocalleader = vim.g.mapleader
+vim.g.loaded_node_provider = 0
 vim.g.loaded_python_provider = 0
 vim.g.loaded_python3_provider = 0
 vim.g.loaded_ruby_provider = 0
